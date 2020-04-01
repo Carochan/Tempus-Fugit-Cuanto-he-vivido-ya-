cf0c08096bc98936c776c485f59968b4f14dc4a1
Caro chan <caro.perezvalenzuela@gmail.com>
2020-04-01 18:25:45 +0200

    El empezose del acabose
    Ahora viene lo serio...

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 /*Esto es mi código ejecutable*/
+const form  = document.getElementsByTagName('tf-data');
+const name = document.getElementById('fName');
+const date = document.getElementById('dBirth').value;
