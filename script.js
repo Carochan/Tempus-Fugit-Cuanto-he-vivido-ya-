6dd9a91df4106cd8d22928371be676078582f5e4
Caro chan <caro.perezvalenzuela@gmail.com>
2020-04-24 19:40:25 +0200

    Update script.js
    Por fin lo saqué XD

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-/*Esto es mi código ejecutable*/
-const form  = document.getElementsByTagName('tf-data');
-const name = document.getElementById('fName');
-const date = document.getElementById('dBirth').value;
+function calculateAndShowDaysOfLife(){
+let nombre = document.getElementById('fName').value;
+let dateOfBirth = document.getElementById('dBirth').valueAsDate;
+
+var end, start;
+
+start = new Date(dateOfBirth);
+for (var i = 0; i < 1000; i++) {
+  Math.sqrt(i);
+}
+end = new Date();
+
+var result= (nombre +' has vivido ' + (Math.floor((end.getTime() - start.getTime())/86400000)) + ' dias.');
+
+document.getElementById("tf-result").innerHTML= '<div class="border-purple-200 border-solid border-4 text-purple rounded-t-lg p-4">' + result + '</div>'
+
+}
