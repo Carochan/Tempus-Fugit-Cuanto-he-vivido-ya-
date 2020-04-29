e7e35d3ffbdd18f7ca299702d5899358a844f39b
Caro chan <caro.perezvalenzuela@gmail.com>
2020-04-29 19:17:17 +0200

    optimización codigo
    Venga que le meto el galimatias del ES6 con ${}

--- script.js
+++ script.js
@@ -10,7 +10,11 @@ for (var i = 0; i < 1000; i++) {
 }
 end = new Date();
 
-let result= ('¡' + nombre +' has vivido la friolera de ' + (Math.floor((end.getTime() - start.getTime())/86400000)) + ' dias!');
+/*let result= ('¡' + nombre +' has vivido la friolera de ' + (Math.floor((end.getTime() - start.getTime())/86400000)) + ' dias!');*/
+
+let result= `¡${nombre} has vivido la friolera de ${Math.floor((end.getTime() - start.getTime())/86400000)} dias!`
+
+
 
 document.getElementById("tf-result").innerHTML= '<div class="bg-orange-500 text-center text-white  p-4">' + result + '</div>'
 
